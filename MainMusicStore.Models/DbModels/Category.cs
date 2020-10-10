1419de60f08ccf30be222762a730cd000157b5e3
Arda Parlak <ardaparlak4@gmail.com>
2020-10-11 00:16:47 +0300

    Repository added

---
+++ MainMusicStore.Models/DbModels/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MainMusicStore.Models.DbModels
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Bu Alan Boş Geçilemez")]
+        [StringLength(250,MinimumLength =3,ErrorMessage ="Şartlara Uygun bir Değergiriniz")]
+        public string CategoryName { get; set; }
+    }
+}
